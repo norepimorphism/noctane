2248210a25c1df6749dc3d91e218715146ed9017
ani <97794085+norepimorphism@users.noreply.github.com>
2022-04-18 08:34:56 -0400

    Reposition license header in asm.rs

--- cpu/src/instr/asm.rs
+++ cpu/src/instr/asm.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use std::fmt;
 
 #[derive(Clone, Debug)]
@@ -21,10 +25,6 @@ impl fmt::Display for Asm {
         )
     }
 }
-
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #[derive(Clone, Debug)]
 pub enum Operand {
