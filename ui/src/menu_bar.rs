59cf91f48828c30a62b0ac35a5b8704fc3ea83c5
ani <97794085+norepimorphism@users.noreply.github.com>
2022-05-18 09:27:32 -0400

    Refactor menu_bar.rs

--- ui/src/menu_bar.rs
+++ ui/src/menu_bar.rs
@@ -7,114 +7,111 @@ use anyhow::Context as _;
 use crate::window::Windows;
 use i18n_embed::fluent::FluentLanguageLoader;
 
+pub struct MenuBar<'b> {
+    pub file: FileMenu<'b>,
+    pub edit: EditMenu<'b>,
+    pub view: ViewMenu<'b>,
+    pub help: HelpMenu<'b>,
+}
+
+pub struct FileMenu<'b> {
+    pub open_iso: &'b mut boing::MenuItem<'b>,
+    pub quit: &'b mut boing::MenuItem<'b>,
+}
+
+pub struct EditMenu<'b> {
+    pub prefs: &'b mut boing::MenuItem<'b>,
+}
+
+pub struct ViewMenu<'b> {
+    pub log: &'b mut boing::MenuItem<'b>,
+}
+
+pub struct HelpMenu<'b> {
+    pub about: &'b mut boing::MenuItem<'b>,
+}
+
 impl MenuBar<'_, '_, '_> {
-    pub fn setup<'b>(
+    pub fn new<'b>(
         i18n: &FluentLanguageLoader,
         boing: &'b boing::Ui,
-        windows: &mut Windows<'b>,
-    ) -> anyhow::Result<()> {
-        let mut this = MenuBar { i18n, boing, windows };
-        this.setup_file_menu()?;
-        this.setup_edit_menu()?;
-        this.setup_view_menu()?;
-        this.setup_help_menu()?;
+    ) -> anyhow::Result<Self> {
+        let mut ctx = Context { i18n, boing };
 
-        Ok(())
+        Ok(MenuBar {
+            file: ctx.create_file_menu()?,
+            edit: ctx.create_edit_menu()?,
+            view: ctx.create_view_menu()?,
+            help: ctx.create_help_menu()?,
+        })
     }
 }
 
-pub struct MenuBar<'l, 'b, 'w> {
-    boing: &'b boing::Ui,
-    windows: &'w mut Windows<'b>,
+struct Context<'l, 'b> {
     i18n: &'l FluentLanguageLoader,
+    boing: &'b boing::Ui,
 }
 
-impl MenuBar<'_, '_, '_> {
+impl Context<'_, '_> {
     fn fl(&self, id: &str) -> String {
         self.i18n.get(id)
     }
 
-    fn setup_file_menu(&mut self) -> anyhow::Result<()> {
+    fn create_file_menu(&mut self) -> anyhow::Result<FileMenu> {
         let menu = self.boing.create_menu(self.fl("menu_file_title"))
             .context("Failed to create \"File\" menu")?;
-        self.setup_open_iso_item(menu)?;
+        let open_iso = self.create_open_iso_item(menu)?;
         menu.push_separator();
-        self.setup_quit_item(menu)?;
+        let quit = self.setup_quit_item(menu)?;
 
-        Ok(())
+        Ok(Self { open_iso, quit })
     }
 
-    fn setup_open_iso_item(&mut self, menu: &boing::Menu) -> anyhow::Result<()> {
-        let item = menu.push_new_item(self.fl("menu-item_open-iso_title"))
-            .context("Failed to append \"Open ISO...\" menu item")?;
-        item.on_clicked(|_| {
-            if let Ok(Some(iso_path)) = native_dialog::FileDialog::new()
-                .add_filter("CD-ROM Image", &["iso"])
-                .show_open_single_file()
-            {
-                let iso = std::fs::File::open(iso_path).unwrap();
-                noctane_cdrom::FileSystem::from_reader(iso).unwrap();
-            }
-        });
-
-        Ok(())
+    fn create_open_iso_item(&mut self, menu: &boing::Menu) -> anyhow::Result<&mut boing::MenuItem> {
+        menu.push_new_item(self.fl("menu-item_open-iso_title"))
+            .context("Failed to append \"Open ISO...\" menu item")
     }
 
-    fn setup_quit_item(&mut self, menu: &boing::Menu) -> anyhow::Result<()> {
-        let item = menu.push_new_item(self.fl("menu-item_quit_title"))
-            .context("Failed to append \"Quit\" menu item")?;
-        item.on_clicked(|_| {
-            // TODO
-        });
-
-        Ok(())
+    fn create_quit_item(&mut self, menu: &boing::Menu) -> anyhow::Result<&mut boing::MenuItem> {
+        menu.push_new_item(self.fl("menu-item_quit_title"))
+            .context("Failed to append \"Quit\" menu item")
     }
 
 
-    fn setup_edit_menu(&mut self) -> anyhow::Result<()> {
+    fn create_edit_menu(&mut self) -> anyhow::Result<EditMenu> {
         let menu = self.boing.create_menu(self.fl("menu_edit_title"))
             .context("Failed to create \"Edit\" menu")?;
-        self.setup_prefs_item(menu)?;
+        let prefs = self.create_prefs_item(menu)?;
 
-        Ok(())
+        Ok(Self { prefs })
     }
 
-    fn setup_prefs_item(&mut self, menu: &boing::Menu) -> anyhow::Result<()> {
-        let item = menu.push_new_item(self.fl("menu-item_prefs_title"))
-            .context("Failed to append \"Preferences...\" menu item")?;
-        item.on_clicked(|_| {
-            let _ = self.windows.prefs.show();
-        });
-
-        Ok(())
+    fn create_prefs_item(&mut self, menu: &boing::Menu) -> anyhow::Result<&mut boing::MenuItem> {
+        menu.push_new_item(self.fl("menu-item_prefs_title"))
+            .context("Failed to append \"Preferences...\" menu item")
     }
 
-    fn setup_view_menu(&mut self) -> anyhow::Result<()> {
+    fn create_view_menu(&mut self) -> anyhow::Result<ViewMenu> {
         let menu = self.boing.create_menu(self.fl("menu_view_title"))
             .context("Failed to create \"View\" menu")?;
-        self.setup_log_item(menu)?;
+        let log = self.create_log_item(menu)?;
 
-        Ok(())
+        Ok(Self { log })
     }
 
-    fn setup_log_item(&mut self, menu: &boing::Menu) -> anyhow::Result<()> {
-        let item = menu.push_new_item(self.fl("menu-item_log_title"))
-            .context("Failed to append \"Log\" menu item")?;
-        item.on_clicked(|_| {
-            let _ = self.windows.log.show();
-        });
-
-        Ok(())
+    fn create_log_item(&mut self, menu: &boing::Menu) -> anyhow::Result<&mut boing::MenuItem> {
+        menu.push_new_item(self.fl("menu-item_log_title"))
+            .context("Failed to append \"Log\" menu item")
     }
 
-    fn setup_help_menu(&mut self) -> anyhow::Result<()> {
+    fn create_help_menu(&mut self) -> anyhow::Result<HelpMenu> {
         let menu = self.boing.create_menu(self.fl("menu_help_title"))
             .context("Failed to create \"Help\" menu")?;
         self.setup_website_item(menu)?;
         menu.push_separator();
-        self.setup_about_item(menu)?;
+        let about = self.create_about_item(menu)?;
 
-        Ok(())
+        Ok(Self { about })
     }
 
     fn setup_website_item(&mut self, menu: &boing::Menu) -> anyhow::Result<()> {
@@ -129,13 +126,8 @@ impl MenuBar<'_, '_, '_> {
         Ok(())
     }
 
-    fn setup_about_item(&mut self, menu: &boing::Menu) -> anyhow::Result<()> {
-        let item = menu.push_new_item(self.fl("menu-item_about_title"))
-            .context("Failed to append \"About\" menu item")?;
-        item.on_clicked(|_| {
-            let _ = self.windows.about.show();
-        });
-
-        Ok(())
+    fn create_about_item(&mut self, menu: &boing::Menu) -> anyhow::Result<&mut boing::MenuItem> {
+        menu.push_new_item(self.fl("menu-item_about_title"))
+            .context("Failed to append \"About\" menu item")
     }
 }
