e57bb62386976c7f1701e115b2c8a543bc3e911b
norepimorphism <97794085+norepimorphism@users.noreply.github.com>
2022-05-24 17:41:41 -0400

    Impl. buffer-based memory of 8 KB

--- mem/src/lib.rs
+++ mem/src/lib.rs
@@ -2,15 +2,22 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#![feature(slice_as_chunks)]
+
 impl Memory {
     pub fn new() -> Self {
         Self::default()
     }
 }
 
-#[derive(Default)]
-pub struct Memory {
+impl Default for Memory {
+    fn default() -> Self {
+        Self { buf: [0; 8192] }
+    }
+}
 
+pub struct Memory {
+    buf: [u8; 8192],
 }
 
 impl Memory {
@@ -18,40 +25,37 @@ impl Memory {
     ///
     /// This function will silently ignore the lowest two bits if `addr` is unaligned.
     pub fn read_32(&mut self, addr: u32) -> u32 {
-        // TODO
-        0
+        u32::from_le_bytes(self.buf.as_chunks::<4>().0[(addr / 4) as usize])
     }
 
     /// Reads the 16-bit value at the given physical address.
     ///
     /// This function will silently ignore the lowest bit if `addr` is unaligned.
     pub fn read_16(&mut self, addr: u32) -> u16 {
-        // TODO
-        0
+        u16::from_be_bytes(self.buf.as_chunks::<2>().0[(addr / 2) as usize])
     }
 
     /// Reads the 8-bit value at the given physical address.
     pub fn read_8(&mut self, addr: u32) -> u8 {
-        // TODO
-        0
+        self.buf[addr as usize]
     }
 
     /// Writes a 32-bit value to the given physical address.
     ///
     /// This function will silently ignore the lowest two bits if `addr` is unaligned.
     pub fn write_32(&mut self, addr: u32, value: u32) {
-        // TODO
+        self.buf.as_chunks_mut::<4>().0[(addr / 4) as usize] = value.to_le_bytes();
     }
 
     /// Writes a 16-bit value to the given physical address.
     ///
     /// This function will silently ignore the lowest bit if `addr` is unaligned.
     pub fn write_16(&mut self, addr: u32, value: u16) {
-        // TODO
+        self.buf.as_chunks_mut::<2>().0[(addr / 2) as usize] = value.to_le_bytes();
     }
 
     /// Writes an 8-bit value to the given physical address.
     pub fn write_8(&mut self, addr: u32, value: u8) {
-        // TODO
+        self.buf[addr as usize] = value;
     }
 }
